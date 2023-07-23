9cf4844f43d1119771169a213999f81fab280343
Deividas Karunos <aserlix@gmail.com>
2023-07-23 20:45:56 +0100

    base html components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/app.css">
+    <title>Document</title>
+    <script src="{{ env('APP_URL') }}/js/app.js" defer></script>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css" rel="stylesheet">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js"></script>
+</head>
+
+<body>
+    <div class="background">
+        <img src="https://thecyberhawk.com/wp-content/uploads/2023/03/iStock-1386914608-new-scaled.jpg"
+            alt="background">
+        {{-- <img src="https://thecyberhawk.com/wp-content/uploads/2023/03/iStock-1362365251-new-scaled.jpg"> --}}
+
+    </div>
